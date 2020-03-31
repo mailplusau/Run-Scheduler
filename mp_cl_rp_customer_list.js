2a31ba517d7bd71a3b79fd0a04517a3e167f9b66
gaellegreiveldinger <59627590+gaellegreiveldinger@users.noreply.github.com>
2020-04-01 10:47:26 +1100

    update
    update the service leg number when deleting a stop
    delete both stops for a transfer

--- mp_cl_rp_customer_list.js
+++ mp_cl_rp_customer_list.js
@@ -1,6 +1,5 @@
 var baseURL = 'https://1048144.app.netsuite.com';
 if (nlapiGetContext().getEnvironment() == "SANDBOX") {
-    console.log('SANDBOX');
     baseURL = 'https://1048144-sb3.app.netsuite.com';
 }
 
